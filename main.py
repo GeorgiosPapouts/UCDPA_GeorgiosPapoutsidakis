75b9a126b0b827a0efb87847f5483d511a9d0227
georgepapoutsidakis <georgios.papoutsidakis@ucdconnect.ie>
2021-04-13 21:48:00 +0100

    13-04-2021 project progress

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import pandas as pd
+
+# Import the one-star-michelin-restaurants.csv data: dining_1
+dining_1 = pd.read_csv('one-star-michelin-restaurants.csv')
+
+# print out dining_1
+print(dining_1)
+
+# Import the one-star-michelin-restaurants.csv data: dining_2
+dining_2 = pd.read_csv('two-stars-michelin-restaurants.csv')
+
+# print out dining_2
+print(dining_2)
+
+# Import the one-star-michelin-restaurants.csv data: dining_3
+dining_3 = pd.read_csv('three-stars-michelin-restaurants.csv')
+
+# print out dining_3
+print(dining_3)
 
